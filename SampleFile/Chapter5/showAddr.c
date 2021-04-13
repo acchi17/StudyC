9a4a8906907025daa09fc1585a6ef5c016a56c27
acchi17 <grf317@gmail.com>
2021-04-13 21:22:24 +0900

    Change

--- SampleFile/Chapter5/showAddr.c
+++ SampleFile/Chapter5/showAddr.c
@@ -8,12 +8,15 @@ int main(void)
     unsigned char *addr1 = &code;
     unsigned char *addr2 = &num;
 
-    printf("Address of \"code\": 0x%x\n", &code);
-    printf("Value   of \"code\": %x\n", *addr1);
-    printf("Address of \"num\" : 0x%x\n", addr2);
-    printf("Value   of \"num\" :\n");
-    printf("%02x\n", *addr2);
-    printf("%02x\n", *(addr2+1));
-    printf("%02x\n", *(addr2+2));
-    printf("%02x\n", *(addr2+3));
+    printf("Address of \"code\": 0x%x\n", addr1);
+    printf("Value   of \"code\": %x\n",  *addr1);
+    printf("\n");
+    printf("Address of \"num\" : 0x%x\n",    addr2);
+    printf("Value   of \"num\" : 0x%02x\n", *addr2);
+    printf("Address of \"num\" : 0x%x\n",    (addr2+1));
+    printf("Value   of \"num\" : 0x%02x\n", *(addr2+1));
+    printf("Address of \"num\" : 0x%x\n",    (addr2+2));
+    printf("Value   of \"num\" : 0x%02x\n", *(addr2+2));
+    printf("Address of \"num\" : 0x%x\n",    (addr2+3));
+    printf("Value   of \"num\" : 0x%02x\n", *(addr2+3));
 }
